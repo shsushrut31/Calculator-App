aa571f2d5c641755b37e1607428e404b43e9086b
Tayzin Mawani <Tayy@Tayzins-MacBook-Air.local>
2017-04-25 18:12:23 -0400

    Calculator app with basic functions

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  CalculatorApp
 //
-//  Created by Tayzin Mawani on 2017-04-25.
+//  Created by Sushrut Shastri on 2017-04-25.
 //  Copyright © 2017 Sushrut Shastri. All rights reserved.
 //
 
@@ -10,16 +10,87 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var firstNumberText = ""
+    var secondNumberText = ""
+    var operation = ""
+    var isFirstNumber = true
+    var calculating = false
+    var clear = true
+    //var middleOfTheString = false
+    
+    @IBOutlet weak var display: UILabel!
+    
+   
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if clear {
+            display.text = ""
+            clear = false
+        }
+        let currentText = display.text!
+        let txtLabel = sender.titleLabel?.text
+        if let text = txtLabel {
+            switch text {
+                //when operators are pressed
+            case "+", "x", "/", "-":
+                if calculating {
+                    return
+                }
+                    operation = text
+                    isFirstNumber = false
+                    calculating = true
+                    display.text = "\(currentText) \(operation) "
+             
+                break
+                //when = is pressed
+            case "=":
+                
+                    isFirstNumber = true
+                    calculating = false
+                    clear = true
+                    //run calculate and display result
+                    let result = calculate()
+                    display.text = "\(result)"
+                break
+                //when Clear pressed
+            case "c":
+                display.text = "0"
+                
+                //when numbers pressed
+            default:
+                //to get numbers for operations
+                if isFirstNumber {
+                    firstNumberText = "\(firstNumberText)\(text)"
+                } else {
+                    secondNumberText = "\(secondNumberText)\(text)"
+                }
+                display.text = "\(currentText)\(text)"
+               
+                break;
+            }
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //function to perform operations
+    func calculate() -> Double {
+        let firstNumber = Double(firstNumberText)!
+        let secondNumber = Double(secondNumberText)!
+        firstNumberText = ""
+        secondNumberText = ""
+        switch operation {
+        case "+":
+            return firstNumber + secondNumber
+        case "-":
+            return firstNumber - secondNumber
+        case "x":
+            return firstNumber * secondNumber
+        case "/":
+            return firstNumber / secondNumber
+        default:
+            return 0
+        }
     }
-
 
 }
 
